6c340608ffd9ea6a6a8e1fd40a9ad22369010c5f
Jochen Pfeffer <jochenpfeffer@Jochens-Air-5.fritz.box>
2025-01-21 23:40:12 +0100

    Update

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ from routes.routes_calc_param import param_calc_bp
 from routes.routes_calc_takt import takt_calc_bp
 from routes.routes_landing import landing_bp
 from routes.routes_account import account_bp
+from routes.legal import legal_bp
 
 def create_app():
     app = Flask(__name__)
@@ -62,6 +63,7 @@ def create_app():
     app.register_blueprint(param_calc_bp, url_prefix="/calc/param")
     app.register_blueprint(takt_calc_bp, url_prefix="/calc/takt")
     app.register_blueprint(account_bp, url_prefix="/account")
+    app.register_blueprint(legal_bp, url_prefix="")
 
     @app.route("/")
     def landing_page():
